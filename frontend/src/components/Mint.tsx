55aff058b43ba0f15c119389394c2a6dc98a71e2
rike <hello@rike.dev>
2022-03-04 15:43:51 +0100

    feat: create possbility to upload a sample React component to fleek/ipfs (canvas to buffer)

--- frontend/src/components/Mint.tsx
+++ frontend/src/components/Mint.tsx
@@ -1,69 +1,61 @@
-import { ethers } from "ethers";
-import contractAbi from "../abi/Maxima.json";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import fleekStorage from "@fleekhq/fleek-storage-js";
 import { v4 as uuidv4 } from "uuid";
-import testpic from "../testpic.png";
+import html2canvas from "html2canvas";
+import { Buffer } from "buffer";
+
 const { REACT_APP_FLEEK_KEY, REACT_APP_FLEEK_SECRET } = process.env;
 
 const Mint = () => {
   const [isMinting, toggleMint] = useState(false);
+  const printRef = useRef() as React.MutableRefObject<HTMLInputElement>;
 
   const askContractToMintNft = async () => {
     toggleMint(true);
-    try {
-      if (REACT_APP_FLEEK_SECRET && REACT_APP_FLEEK_KEY) {
-        const date = new Date();
-        const timestamp = date.getTime();
-        const newTokenId = uuidv4();
-        const input = await fleekStorage.upload({
-          apiKey: REACT_APP_FLEEK_KEY,
-          apiSecret: REACT_APP_FLEEK_SECRET,
-          key: `nft/${newTokenId}-${timestamp}`,
-          data: testpic,
-        });
-        console.log(input);
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+
+    const saveToIpfs = async (reader: any) => {
+      const buffer = Buffer.from(reader.result);
+      console.log(buffer);
+      try {
+        if (REACT_APP_FLEEK_SECRET && REACT_APP_FLEEK_KEY) {
+          const date = new Date();
+          const timestamp = date.getTime();
+          const newTokenId = uuidv4();
+          const input = await fleekStorage.upload({
+            apiKey: REACT_APP_FLEEK_KEY,
+            apiSecret: REACT_APP_FLEEK_SECRET,
+            key: `nft/${newTokenId}-${timestamp}`,
+            data:  buffer,
+          });
+          console.log(input);
+          // after final uploading to fleek, next step would be
+          // to trigger connectedContract.makeWordleNFT() here, but with
+          // image fleek/ipfs URI
+          toggleMint(false);
+        }
+      } catch (e) {
+        console.error(e);
         toggleMint(false);
-        // createNFTTransaction(hash)
       }
-    } catch (e) {
-      console.error(e);
-      toggleMint(false);
-    }
-    // try {
-    //   const { ethereum } = window;
-
-    //   if (ethereum) {
-    //     const provider = new ethers.providers.Web3Provider(ethereum);
-    //     const signer = provider.getSigner();
-    //     const connectedContract = new ethers.Contract(
-    //       "0x7b7127Da4419656e2D76Ec3104605ba7B1F29Ca1",
-    //       contractAbi.abi,
-    //       signer
-    //     );
-
-    //     console.log("Going to pop wallet now to pay gas...");
-    //     let nftTxn = await connectedContract.makeWordleNFT();
-
-    //     console.log("Mining...please wait.");
-    //     await nftTxn.wait();
-    //     console.log(nftTxn);
-    //     console.log(
-    //       `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
-    //     );
-    //     toggleMint(false);
-    //   } else {
-    //     console.log("Ethereum object doesn't exist!");
-    //     toggleMint(false);
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    //   toggleMint(false);
-    // }
+    };
+    console.log(canvas);
+    canvas.toBlob(function (blob) {
+      if (blob) {
+        let reader: any = new FileReader();
+        reader.onloadend = () => saveToIpfs(reader);
+        reader.readAsArrayBuffer(blob);
+      }
+    });
   };
 
   return (
     <div className="max-w-4xl mx-auto md:px-1 px-3">
+      <div style={{ background: "purple", height: 200 }} ref={printRef}>
+        I will be in the image.
+      </div>
+      <br />
       {!isMinting ? (
         <button
           className="bg-teal-600 hover:bg-teal-500 hover:text-white active:bg-teal-500  text-white font-bold py-2 px-4 rounded-full"
