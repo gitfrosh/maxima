62544d7b45c60bc70b0290758f6698ff3481212f
rike <hello@rike.dev>
2022-03-05 19:19:01 +0100

    fix

--- frontend/src/components/modals/StatsModal.tsx
+++ frontend/src/components/modals/StatsModal.tsx
@@ -75,7 +75,7 @@ export const StatsModal = ({
             type="button"
             className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-black hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
             onClick={() => {
-             const raw = shareStatus(
+              shareStatus(
                 guesses,
                 isGameLost,
                 isHardMode,
@@ -83,12 +83,10 @@ export const StatsModal = ({
                 isHighContrastMode,
                 handleShareToClipboard
               )
-            setResult(raw);
             }}
           >
             {SHARE_TEXT}
           </button>
-          {result}
         </div>
       )}
     </BaseModal>
