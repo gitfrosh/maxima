dbe3051a45ea22a050a7bd82f84b9174476923df
rike <hello@rike.dev>
2022-03-06 23:40:10 +0100

    add people info

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -32,13 +32,27 @@ const About = () => {
           >
             <path d="M130.7 313.9C126.5 300.4 137.8 288 151.1 288H364.5C378.7 288 389.9 300.4 385.8 313.9C368.1 368.4 318.2 408 258.2 408C198.2 408 147.5 368.4 130.7 313.9V313.9zM208.4 192C208.4 209.7 194 224 176.4 224C158.7 224 144.4 209.7 144.4 192C144.4 174.3 158.7 160 176.4 160C194 160 208.4 174.3 208.4 192zM281.9 214.6C273.9 207 273.5 194.4 281 186.3C295.6 170.8 316.3 164 335.6 164C354.1 164 375.7 170.8 390.2 186.3C397.8 194.4 397.4 207 389.3 214.6C381.2 222.1 368.6 221.7 361 213.7C355.6 207.8 346.3 204 335.6 204C324.1 204 315.7 207.8 310.2 213.7C302.7 221.7 290 222.1 281.9 214.6zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z" />
           </svg>
-          <h3 className="pt-3 font-bold text-lg text-[#FFFFFF]">Member #1</h3>
+          <h3 className="pt-3 font-bold text-lg text-[#FFFFFF]">Ana</h3>
+          <p className="pt-2 value-text text-md text-[#FFFFFF] fkrr1">..</p>
+        </div>
+        <div className="ktq4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 512 512"
+          >
+            <path d="M130.7 313.9C126.5 300.4 137.8 288 151.1 288H364.5C378.7 288 389.9 300.4 385.8 313.9C368.1 368.4 318.2 408 258.2 408C198.2 408 147.5 368.4 130.7 313.9V313.9zM208.4 192C208.4 209.7 194 224 176.4 224C158.7 224 144.4 209.7 144.4 192C144.4 174.3 158.7 160 176.4 160C194 160 208.4 174.3 208.4 192zM281.9 214.6C273.9 207 273.5 194.4 281 186.3C295.6 170.8 316.3 164 335.6 164C354.1 164 375.7 170.8 390.2 186.3C397.8 194.4 397.4 207 389.3 214.6C381.2 222.1 368.6 221.7 361 213.7C355.6 207.8 346.3 204 335.6 204C324.1 204 315.7 207.8 310.2 213.7C302.7 221.7 290 222.1 281.9 214.6zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z" />
+          </svg>
+          <h3 className="pt-3 font-bold text-lg text-[#FFFFFF]">
+            Kristen (CuddleofDeath)
+          </h3>
           <p className="pt-2 value-text text-md text-[#FFFFFF] fkrr1">
-            This is a section in which we can fill out with a little bit about
-            ourselves. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Eveniet quas sed consequuntur molestiae maiores eos excepturi sequi,
-            dicta delectus dolores earum dolore. Quas modi laborum, quae libero
-            ab culpa molestiae!
+            CuddleofDeath (Kristen) is an aspiring full-stack developer
+            currently focusing on learning front-end development. She has
+            recently gotten into the NFT space as of September 2021. Her hobbies
+            include playing video games, streaming, and taking care of her
+            furbabies.
           </p>
         </div>
         <div className="ktq4">
@@ -50,31 +64,15 @@ const About = () => {
           >
             <path d="M130.7 313.9C126.5 300.4 137.8 288 151.1 288H364.5C378.7 288 389.9 300.4 385.8 313.9C368.1 368.4 318.2 408 258.2 408C198.2 408 147.5 368.4 130.7 313.9V313.9zM208.4 192C208.4 209.7 194 224 176.4 224C158.7 224 144.4 209.7 144.4 192C144.4 174.3 158.7 160 176.4 160C194 160 208.4 174.3 208.4 192zM281.9 214.6C273.9 207 273.5 194.4 281 186.3C295.6 170.8 316.3 164 335.6 164C354.1 164 375.7 170.8 390.2 186.3C397.8 194.4 397.4 207 389.3 214.6C381.2 222.1 368.6 221.7 361 213.7C355.6 207.8 346.3 204 335.6 204C324.1 204 315.7 207.8 310.2 213.7C302.7 221.7 290 222.1 281.9 214.6zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z" />
           </svg>
-          <h3 className="pt-3 font-bold text-lg text-[#FFFFFF]">Member #2</h3>
+          <h3 className="pt-3 font-bold text-lg text-[#FFFFFF]">Rike Exner</h3>
           <p className="pt-2 value-text text-md text-[#FFFFFF] fkrr1">
-            This is a section in which we can fill out with a little bit about
-            ourselves. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Eveniet quas sed consequuntur molestiae maiores eos excepturi sequi,
-            dicta delectus dolores earum dolore. Quas modi laborum, quae libero
-            ab culpa molestiae!
-          </p>
-        </div>
-        <div className="ktq4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 512 512"
-          >
-            <path d="M130.7 313.9C126.5 300.4 137.8 288 151.1 288H364.5C378.7 288 389.9 300.4 385.8 313.9C368.1 368.4 318.2 408 258.2 408C198.2 408 147.5 368.4 130.7 313.9V313.9zM208.4 192C208.4 209.7 194 224 176.4 224C158.7 224 144.4 209.7 144.4 192C144.4 174.3 158.7 160 176.4 160C194 160 208.4 174.3 208.4 192zM281.9 214.6C273.9 207 273.5 194.4 281 186.3C295.6 170.8 316.3 164 335.6 164C354.1 164 375.7 170.8 390.2 186.3C397.8 194.4 397.4 207 389.3 214.6C381.2 222.1 368.6 221.7 361 213.7C355.6 207.8 346.3 204 335.6 204C324.1 204 315.7 207.8 310.2 213.7C302.7 221.7 290 222.1 281.9 214.6zM512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 48C141.1 48 48 141.1 48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48z" />
-          </svg>
-          <h3 className="pt-3 font-bold text-lg text-[#FFFFFF]">Member #3</h3>
-          <p className="pt-2 value-text text-md text-[#FFFFFF] fkrr1">
-            This is a section in which we can fill out with a little bit about
-            ourselves. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Eveniet quas sed consequuntur molestiae maiores eos excepturi sequi,
-            dicta delectus dolores earum dolore. Quas modi laborum, quae libero
-            ab culpa molestiae!
+            Rike Exner is a software developer from Berlin, Germany, focusing on
+            web technologies, holding a Master's degree in computer science. She
+            has also worked as an online content manager, librarian, and
+            freelancer and gives workshops for beginners in web development to
+            inspire more women to get into coding. In her remaining time, she
+            contributes to the Open Source community and is the maintainer of
+            the non-profit public Lord of the Rings API.
           </p>
         </div>
 
@@ -132,7 +130,8 @@ const About = () => {
               href="https://www.github.com/afoma"
             >
               Github
-            </a>.
+            </a>
+            .
           </p>
         </div>
       </div>
