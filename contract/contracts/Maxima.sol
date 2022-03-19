2e37f8e6749c5e9405db481035db05e007de5fd5
rike <hello@rike.dev>
2022-03-19 15:29:16 +0100

    fix bug

--- contract/contracts/Maxima.sol
+++ contract/contracts/Maxima.sol
@@ -15,38 +15,40 @@ contract Maxima is ERC721, ERC721URIStorage, Ownable {
     Counters.Counter private _tokenIdCounter;
 
     event NewWordleNFTMinted(address sender, uint256 tokenId);
+
     /// @dev Base token URI used as a prefix by tokenURI().
 
-    constructor() ERC721("WordleNFT", "MAXIMA") {
-    }
+    constructor() ERC721("WordleNFT", "MAXIMA") {}
 
-    function mintAndDonate(
-        address payable _to,
-        string memory metadataURI
-    ) public payable returns (uint256) {
-        require(msg.value >= 0.00001 ether, 'Be nice and donate!');
+    function mintAndDonate(address payable _to, string memory metadataURI)
+        public
+        payable
+        returns (uint256)
+    {
+        require(msg.value >= 0.00001 ether, "Be nice and donate!");
         uint256 newItemId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, newItemId);
         _setTokenURI(newItemId, metadataURI);
-        // replace by address that is not mine
         bool sent = _to.send(msg.value);
         require(sent, "Failed to send Ether");
         return newItemId;
     }
 
-
     // The following functions are overrides required by Solidity.
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
         super._burn(tokenId);
     }
+
     function tokenURI(uint256 tokenId)
-    public
-    view
-    override(ERC721, ERC721URIStorage)
-    returns (string memory)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
     {
         return super.tokenURI(tokenId);
     }
-
 }
